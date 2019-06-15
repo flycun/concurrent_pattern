febff8a552f56c1a5a68a187f13610fad5af0023
flycun <flycun@163.com>
2019-06-15 16:17:38 +0800

    new Runnable

--- src/threadpermessage/sample/Host.java
+++ src/threadpermessage/sample/Host.java
@@ -4,11 +4,12 @@ public class Host {
     private final Helper helper = new Helper();
     public void request(final int count, final char c) {
         System.out.println("    request(" + count + ", " + c + ") BEGIN");
-        new Thread() {
+        new Thread(new Runnable() {
+            @Override
             public void run() {
-                helper.handle(count, c);
+                helper.handle(count,c);
             }
-        }.start();
+        }).start();
 
         System.out.println("    request(" + count + ", " + c + ") END");
     }
