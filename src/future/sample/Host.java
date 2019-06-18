234fe5f7f18915173f0f86793f82fb571c2a196b
flycun <flycun@163.com>
2019-06-18 22:07:54 +0800

    add FutureTask

--- src/future/sample/Host.java
+++ src/future/sample/Host.java
@@ -1,19 +1,23 @@
 package future.sample;
 
+import java.util.concurrent.Callable;
+
 public class Host {
-    public Data request(final int count, final char c) {
+    public FutureData request(final int count, final char c) {
         System.out.println("    request(" + count + ", " + c + ") BEGIN");
 
         // (1) 创建FutureData的实例
-        final FutureData future = new FutureData();
+        //     （向构造函数中传递 Callable<RealData>）
+        FutureData future = new FutureData(
+            new Callable<RealData>() {
+                public RealData call() {
+                    return new RealData(count, c);
+                }
+            }
+        );
 
         // (2) 启动一个新线程，用于创建RealData的实例
-        new Thread() {
-            public void run() {
-                RealData realdata = new RealData(count, c);
-                future.setRealData(realdata);
-            }
-        }.start();
+        new Thread(future).start();
 
         System.out.println("    request(" + count + ", " + c + ") END");
 
